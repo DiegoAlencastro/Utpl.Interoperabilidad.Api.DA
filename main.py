9dee149fd0cd2e31a8a2ea2fc5013f03afdcb42b
Diego Alencastro <alencastrobi@gmail.com>
2023-06-30 16:44:46 +0000

    da

--- main.py
+++ main.py
@@ -9,125 +9,144 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 from auth import authenticate
 
-#seccion mongo_importar libreria
+#seccion mongo importar libreria
 import pymongo
 
+import spotipy
+
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyClientCredentials(
+    client_id='c8519595485648c3949369793de3e366',
+    client_secret='d266e54ea24346a7b278445be87cd400'
+))
 
 description = """
-Interoperabilidad Actualizacion Datos Cliente. 🚀
+Utpl tnteroperabilidad API ayuda a describir las capacidades de un directorio. 🚀
 
-## Cliente
+## Personas
 
-Se actuliza datos cliente.
-Detalle de clientes.
+Tu puedes crear una persona.
+Tu puedes listar personas.
 
+
+## Artistas
+
+You will be able to:
+
+* **Crear artista** (_not implemented_).
 """
+
 tags_metadata = [
     {
-        "name":"clientes",
-        "description":"Actualizacion Datos Cliente"
-    }
+        "name":"personas",
+        "description": "Permite realizar un crud completo de una persona (listar)"
+    },
+    {
+        "name":"artistas",
+        "description": "Permite realizar un crud completo de un artista"
+    },
 ]
+
 app = FastAPI(
-    title="Interoperabilidad Actualizacion Datos Cliente",
+    title="Utpl Interoperabilidad APP 2",
     description= description,
-    version="tarea4",
+    version="0.0.1",
     terms_of_service="http://example.com/terms/",
     contact={
-        "name": "Diego Alencastro",
-        "url": "https://github.com/DiegoAlencastro/Utpl.Interoperabilidad.Api.DA.git",
-        "email": "dialencastro@utpl.edu.ec",
+        "name": "Felipe Quiñonez",
+        "url": "http://x-force.example.com/contact/",
+        "email": "fdquinones@utpl.edu.ec",
     },
     license_info={
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
-    openapi_tags = tags_metadata  
+    openapi_tags = tags_metadata
 )
 
 #para agregar seguridad a nuestro api
 security = HTTPBasic()
 
-
 #configuracion de mongo
-cliente = pymongo.MongoClient("mongodb+srv://utplda:s4nN15Zcbf5W0D5v@cluster0.po6e08w.mongodb.net/?retryWrites=true&w=majority")
-database = cliente["clientes"]
-coleccion = database["datos"]
+cliente = pymongo.MongoClient("mongodb+srv://utplinteroperabilidad:0b1Fd3PFZZInSuZK@cluster0.susnphb.mongodb.net/?retryWrites=true&w=majority")
+database = cliente["directorio"]
+coleccion = database["persona"]
 
-class Cliente (BaseModel):
-    orden: int
-    nombre: str
-    edad: int
-    agencia: Optional[str] = None
-    cuenta: int
-
-
-class ClienteRepositorio (BaseModel):
+class PersonaRepositorio (BaseModel):
     id: str
     nombre: str
-    apellido: str
     edad: int
-    oficina: Optional[str] = None
+    identificacion: Optional[str] = None
+    ciudad: Optional[str] = None
 
-class ClienteEntrada (BaseModel):
-    id: str
-    nombre: str
-    apellido: str
-    edad: int
-    oficina: Optional[str] = None
+class PersonaEntrada (BaseModel):
+    nombre:str
+    edad:int
+    ciudad: Optional[str] = None
 
-personasList = []
+class PersonaEntradaV2 (BaseModel):
+    nombre:str
+    edad:int
+    identificacion:str
+    ciudad: Optional[str] = None
 
-@app.post("/clientes", response_model=ClienteRepositorio, tags = ["clientes"])
-@version(1,0)
-async def crear_cliente(clientE: ClienteEntradaEntrada):
-    print ('llego')
-    clientE.apellido, edad = clientE.edad, oficina = clientE.oficina)
-    resultadoDB =  coleccion.insert_one(itemCliente.dict())
-    return itemCliente
 
-@app.get("/clientes", response_model=List[ClienteRepositorio], tags=["clientes"])
-@version(1,0)
-def get_clientes():
-    itemsCliente = list(coleccion.find())
+personaList = []
+
+@app.post("/personas", response_model=PersonaRepositorio, tags = ["personas"])
+@version(1, 0)
+async def crear_persona(personE: PersonaEntrada):
+    itemPersona = PersonaRepositorio (id= str(uuid.uuid4()), nombre = personE.nombre, edad = personE.edad, ciudad = personE.ciudad)
+    resultadoDB =  coleccion.insert_one(itemPersona.dict())
+    return itemPersona
+
+@app.post("/personas", response_model=PersonaRepositorio, tags = ["personas"])
+@version(2, 0)
+async def crear_personav2(personE: PersonaEntradaV2):
+    itemPersona = PersonaRepositorio (id= str(uuid.uuid4()), nombre = personE.nombre, edad = personE.edad, ciudad = personE.ciudad, identificacion = personE.identificacion)
+    resultadoDB =  coleccion.insert_one(itemPersona.dict())
+    return itemPersona
+
+@app.get("/personas", response_model=List[PersonaRepositorio], tags=["personas"])
+@version(1, 0)
+def get_personas(credentials: HTTPBasicCredentials = Depends(security)):
+    authenticate(credentials)
+    items = list(coleccion.find())
     print (items)
-    return itemsCliente
+    return items
 
-## Buscar Cliente
-@app.get("/clientes/{cliente_id}", response_model=ClienteRepositorio, tags=["clientes"])
-@version(1,0)
-def obtener_cliente (cliente_id: str):
+@app.get("/personas/{persona_id}", response_model=PersonaRepositorio , tags=["personas"])
+@version(1, 0)
+def obtener_persona (persona_id: str):
     item = coleccion.find_one({"id": persona_id})
     if item:
         return item
     else:
-        raise HTTPException(status_code=404, detail="Cliente no Existe")
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
 
-## Identificar Cliente por codigo.    
-@app.get("/Cliente/codigo/{cod_num}", response_model=Huesped, tags = ["clientes"])
-@version(2,0)
-def obtener_cod(cod_num: int):
-    item = coleccion.find_one({"cod": cod_num})
-    if item:
-        return item
-    else:
-        raise HTTPException(status_code=404, detail="Cliente no Existe")
-
-    ##codigo Sin Existencia
-    
-@app.delete("/clientes/{cliente_id}", tags=["clientes"])
-@version(1,0)
-def eliminar_cliente (cliente_id: str):
-    result = coleccion.delete_one({"id": cliente_id})
+@app.delete("/personas/{persona_id}", tags=["personas"])
+@version(1, 0)
+def eliminar_persona (persona_id: str):
+    result = coleccion.delete_one({"id": persona_id})
     if result.deleted_count == 1:
-        return {"mensaje": "Eliminado exitosamente"}
+        return {"mensaje": "Persona eliminada exitosamente"}
     else:
-        raise HTTPException(status_code=404, detail="Cliente no Existe")
+        raise HTTPException(status_code=404, detail="Persona no encontrada")
+
+@app.get("/pista/{pista_id}", tags = ["artistas"])
+@version(1, 0)
+async def obtener_pista(pista_id: str):
+    track = sp.track(pista_id)
+    return track
+    
+@app.get("/artistas/{artista_id}", tags = ["artistas"])
+@version(1, 0)
+async def get_artista(artista_id: str):
+    artista = sp.artist(artista_id)
+    return artista
+
 
 @app.get("/")
-@version(1,0)
 def read_root():
-    return {"Hello": "Tarea Concluida APP Cliente DA"}
-
+    return {"Hello": "Interoperabilidad 8"}
 
 app = VersionedFastAPI(app)
